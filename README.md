6a4ad04a5aa5311fb9e784f54929e15434539132
Devilbinder <Devilbinder@users.noreply.github.com>
2020-01-07 17:38:45 +0200

    readme and compiler

--- README.md
+++ README.md
@@ -3,17 +3,17 @@ This video Demonstrates how to setup the UART in asynchronous 8-bit mode. Using 
 
 [![PIC Programming Tutorial #7 - UART](https://img.youtube.com/vi/pPmEWlIYByo/0.jpg)](https://www.youtube.com/watch?v=MiPxVmj_nHI "PIC Programming Tutorial #5 - Interrupts")
 
-<a href="https://amzn.to/2CzX8Wc">FTDI USB to Serial</a>
 
-<a href="https://amzn.to/2oTHRqm">PIC18f4520</a>
 
-<a href="https://amzn.to/2p2PsmV">PIC18f4520  (Alternative)</a>
 
-<a href="https://amzn.to/2x5Fq8a">LED Kit</a>
 
-<a href="https://amzn.to/2COwEBA">Resistor Kit</a>
 
-<a href="https://amzn.to/2BzKsiE">Pickit3 Programmmer (compatible)</a>
 
-<a href="https://www.microchip.com/Developmenttools/ProductDetails/PartNo/PG164130">Pickit3 Programmmer (original)</a>
+
+
+
+
+
+
+
 
--- uart.X/main.c
+++ uart.X/main.c
@@ -1,5 +1,5 @@
 #include <xc.h>
-#include <p18f4520.h>
+#include <pic18f4520.h>
 #include "conbits.h"
 #include "stdint.h"
 #include "stdbool.h"
@@ -7,8 +7,8 @@
 uint8_t data = 'A';
 bool got_data_bool = false;
 
-void interrupt high_isr(void);
-void interrupt low_priority low_isr(void);
+void __interrupt() high_isr(void);
+void __interrupt(low_priority) low_isr(void);
 
 
 void uart_init(uint16_t gen_reg, unsigned sync,unsigned brgh, unsigned brg16){
@@ -80,7 +80,7 @@ void main(void) {
     } 
 }
 
-void interrupt high_isr(void){
+void __interrupt() high_isr(void){
     INTCONbits.GIEH = 0;
     if(PIR1bits.RCIF){
         uart_receiver(&data,&got_data_bool);
@@ -90,7 +90,7 @@ void interrupt high_isr(void){
     INTCONbits.GIEH = 1;
 }
 
-void interrupt low_priority low_isr(void){
+void __interrupt(low_priority) low_isr(void){
     INTCONbits.GIEH = 0;
     if(PIR1bits.TXIF){
         PIR1bits.TXIF=0;
